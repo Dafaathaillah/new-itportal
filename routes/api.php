f23a15adc8c673b2779732de8b607a30d74aec75
dafaathaillah <dafabintang76@gmail.com>
2024-07-03 10:27:09 +0800

    update routes API add new group peminjaman

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ComputerLoanController;
 use App\Http\Controllers\InvApController;
 use App\Http\Controllers\InvCctvController;
+use App\Http\Controllers\InvChannelRadioController;
 use App\Http\Controllers\InvComputerController;
 use App\Http\Controllers\InvGpsController;
 use App\Http\Controllers\InvLaptopController;
@@ -10,10 +12,12 @@ use App\Http\Controllers\InvNvrController;
 use App\Http\Controllers\InvPanelBoxController;
 use App\Http\Controllers\InvPrinterController;
 use App\Http\Controllers\InvRadioController;
+use App\Http\Controllers\InvServerController;
 use App\Http\Controllers\InvSwitchController;
 use App\Http\Controllers\InvTowerController;
 use App\Http\Controllers\InvWirellessController;
 use App\Http\Controllers\KomputerController;
+use App\Http\Controllers\LaptopLoanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -38,4 +42,11 @@ Route::prefix('inventory')->group(function () {
     Route::apiResource('panel_box', InvPanelBoxController::class);   
     Route::apiResource('gps', InvGpsController::class);   
     Route::apiResource('radio', InvRadioController::class);   
+    Route::apiResource('channel_radio', InvChannelRadioController::class);   
+    Route::apiResource('server', InvServerController::class);   
+});
+
+Route::prefix('peminjaman')->group(function () {
+    Route::apiResource('komputer', ComputerLoanController::class);   
+    Route::apiResource('laptop', LaptopLoanController::class);   
 });
