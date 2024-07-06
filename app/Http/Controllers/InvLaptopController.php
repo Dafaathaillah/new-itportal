0fcc170cb67017ea0691f3a87019d4e1cb8feff9
dafaathaillah <dafabintang76@gmail.com>
2024-07-06 14:08:33 +0800

    update controller inventory laptop add generate code with date

--- app/Http/Controllers/InvLaptopController.php
+++ app/Http/Controllers/InvLaptopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\InvLaptop;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class InvLaptopController extends Controller
@@ -15,6 +16,22 @@ class InvLaptopController extends Controller
 
     public function store(Request $request)
     {
+        // start generate code
+        $currentDate = Carbon::now();
+        $year = $currentDate->format('y');
+        $month = $currentDate->month;
+        $day = $currentDate->day;
+
+        $maxId = InvLaptop::max('id');
+
+        if (is_null($maxId)) {
+            $maxId = 0;
+        }
+
+        $uniqueString = 'PPABIBNB' . $year . $month . $day . '-' . str_pad(($maxId % 10000) + 1, 2, '0', STR_PAD_LEFT);
+        $request['laptop_code'] = $uniqueString;
+        // end generate code
+
         $invLaptop_get_data = InvLaptop::find($request->id);
         if (empty($invLaptop_get_data)) {
             $invLaptop = InvLaptop::create($request->all());
@@ -23,7 +40,6 @@ class InvLaptopController extends Controller
             $invLaptop = InvLaptop::firstWhere('id', $request->id)->update($request->all());
             return response()->json($invLaptop, 201);
         }
-
     }
 
     public function show($id)
