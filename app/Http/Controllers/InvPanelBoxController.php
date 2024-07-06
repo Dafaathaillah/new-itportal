2d318c5c2b812bd89fce00d439be39f4ca5c10a9
dafaathaillah <dafabintang76@gmail.com>
2024-07-06 14:09:09 +0800

    update controller inventory panel box add generate code with date

--- app/Http/Controllers/InvPanelBoxController.php
+++ app/Http/Controllers/InvPanelBoxController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\InvPanelBox;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class InvPanelBoxController extends Controller
@@ -15,6 +16,22 @@ class InvPanelBoxController extends Controller
 
     public function store(Request $request)
     {
+         // start generate code
+         $currentDate = Carbon::now();
+         $year = $currentDate->format('y');
+         $month = $currentDate->month;
+         $day = $currentDate->day;
+ 
+         $maxId = InvPanelBox::max('id');
+ 
+         if (is_null($maxId)) {
+             $maxId = 0;
+         }
+ 
+         $uniqueString = 'PBN' . $year . $month . $day . '-' . str_pad(($maxId % 10000) + 1, 2, '0', STR_PAD_LEFT);
+         $request['inventory_number'] = $uniqueString;
+         // end generate code
+
         $invPanelBox_get_data = InvPanelBox::find($request->id);
         if (empty($invPanelBox_get_data)) {
             $invPanelBox = InvPanelBox::create($request->all());
