2b1b33b1176a87050889a534a0aa956fd572ff92
dafaathaillah <dafabintang76@gmail.com>
2024-07-06 14:08:15 +0800

    update controller inventory computer add generate code with date

--- app/Http/Controllers/InvComputerController.php
+++ app/Http/Controllers/InvComputerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\InvComputer;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class InvComputerController extends Controller
@@ -15,6 +16,23 @@ class InvComputerController extends Controller
 
     public function store(Request $request)
     {
+        // start generate code
+        $currentDate = Carbon::now();
+        $year = $currentDate->format('y');
+        $month = $currentDate->month;
+        $day = $currentDate->day;
+
+        $maxId = InvComputer::max('id');
+
+        if (is_null($maxId)) {
+            $maxId = 0;
+        }
+
+        $uniqueString = 'PPABIBCU' . $year . $month . $day . '-' . str_pad(($maxId % 10000) + 1, 2, '0', STR_PAD_LEFT);
+        $request['computer_code'] = $uniqueString;
+        // end generate code
+
+
         $invComputers_get_data = InvComputer::find($request->id);
         if (empty($invComputers_get_data)) {
             $invComputers = InvComputer::create($request->all());
@@ -23,7 +41,6 @@ class InvComputerController extends Controller
             $invComputers = InvComputer::firstWhere('id', $request->id)->update($request->all());
             return response()->json($invComputers, 201);
         }
-
     }
 
     public function show($id)
