01f11c16697aa192a4d512fb229398dd37da5a4c
dafaathaillah <dafabintang76@gmail.com>
2024-07-10 14:22:33 +0800

    update code controller updtae data add record data

--- app/Http/Controllers/GaransionLaptopController.php
+++ app/Http/Controllers/GaransionLaptopController.php
@@ -71,27 +71,41 @@ class GaransionLaptopController extends Controller
                 $garansiLaptop = GaransionLaptop::create($dataGaransionAdd);
                 return response()->json(['message' => 'Berhasil create data perangkat breakdown garansi laptop'], 201);
             } else {
+                $date = Carbon::now();
+                $month = $date->format('m');
+                $year = $date->format('Y');
+                $data_garansi_laptop_prev = DB::table('garansion_laptops')->where('garansion_code', $request->garansion_laptop_code)->first();
+
+                $validatedDataGaransi = $request->validate([
+                    'laptop_id' => 'nullable|string|max:255',
+                    'inventory_number' => 'nullable|string|max:255',
+                    'status' => 'nullable|string|max:255',
+                    'start_progress' => 'nullable|date_format:Y-m-d H:i:s',
+                    'end_progress' => 'nullable|date_format:Y-m-d H:i:s',
+                    'date_of_garansion' => 'nullable|date_format:Y-m-d H:i:s',
+                    'hardware_damage' => 'nullable|string|max:255',
+                ]);
+                $validatedDataGaransi['month'] = $month;
+                $validatedDataGaransi['year'] = $year;
+                $validatedDataGaransi['record_data'] = $data_garansi_laptop_prev->record_data . ' -> ' . $request->date_of_garansion . ' ' . $request->status . ' -> ';
+
+
                 $validatedDataPerangkatBreakdown = $request->validate([
                     'inventory_number' => 'nullable|string|max:255',
                     'start_progress' => 'nullable|date_format:Y-m-d H:i:s',
                     'end_progress' => 'nullable|date_format:Y-m-d H:i:s',
                     'status' => 'nullable|string|max:255',
                 ]);
-                $date = Carbon::now();
-                $month = $date->format('m');
-                $year = $date->format('Y');
 
                 // $validatedDataPerangkatBreakdown['garansion_laptop_code'] = $uniqueString;
                 $validatedDataPerangkatBreakdown['location'] = $data_inv_laptop->location;
                 $validatedDataPerangkatBreakdown['pic'] = 'DAFA BINTANG ATHAILLAH';
                 $validatedDataPerangkatBreakdown['month'] = $month;
                 $validatedDataPerangkatBreakdown['year'] = $year;
-                $dataGaransionAdd = $request->all();
-                $dataGaransionAdd['month'] = $month;
-                $dataGaransionAdd['year'] = $year;
 
                 $perangkatBreakdown = PerangkatBreakdown::firstWhere('garansion_laptop_code', $request->garansion_laptop_code)->update($validatedDataPerangkatBreakdown);
-                $garansiLaptop = GaransionLaptop::firstWhere('garansion_code', $request->garansion_laptop_code)->update($request->all());
+                $garansiLaptop = GaransionLaptop::firstWhere('garansion_code', $request->garansion_laptop_code)->update($validatedDataGaransi);
+                // return response()->json($garansiLaptop);
                 return response()->json(['message' => ' data perangkat breakdown dan garansi laptop berhasil di update'], 201);
             }
         }
