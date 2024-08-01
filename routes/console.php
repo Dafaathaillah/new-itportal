3376ae69189477c0580424af03e49ac57769d3c1
dafaathaillah <dafabintang76@gmail.com>
2024-08-01 08:44:05 +0800

    schedule import data inspeksi computer and laptop

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+// use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+// Schedule::command('inspeksiComputer:cron')->everyMinute(); //  * * * * * php /path-to-your-project/artisan schedule:run >> /dev/null 2>&1 //
+// Schedule::command('inspeksiLaptop:cron')->everyMinute(); // * * * * * php /path/to/your/project/artisan schedule:run >> /dev/null 2>&1 //
+Schedule::command('inspeksiComputer:cron')->quarterly(); //  * * * * * php /path-to-your-project/artisan schedule:run >> /dev/null 2>&1 //
+Schedule::command('inspeksiLaptop:cron')->yearly(); // * * * * * php /path/to/your/project/artisan schedule:run >> /dev/null 2>&1 //
