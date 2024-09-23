d6d2e9d9403adb0680946fd1de76d2fdf202f1e7
dikrix <dikriardiansyah@gmail.com>
2024-09-23 08:00:10 +0800

    laravel breeze, layouting dan api documentation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,32 +8,50 @@ use App\Http\Middleware\HandleInertiaRequests;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'api' => [
-                \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-                'throttle:api',
-                \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            ],
-            'type.ict_developer' => \App\Http\Middleware\IctDeveloperMiddleware::class,
-            'type.ict_section_head' => \App\Http\Middleware\IctSectionHead::class,
-            'type.ict_group_leader' => \App\Http\Middleware\IctGroupLeader::class,
-            'type.ict_admin' => \App\Http\Middleware\IctAdmin::class,
-            'type.ict_technician' => \App\Http\Middleware\IctTechnician::class,
-            // 'type.ict_guest' => \App\Http\Middleware\AdminMiddleware::class,
+        $middleware->web(append: [
+            HandleInertiaRequests::class,
+            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        // $middleware->alias([
+        //     'api' => [
+        //         \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        //         'throttle:api',
+        //         \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        //     ],
+        //     'type.ict_developer' => \App\Http\Middleware\IctDeveloperMiddleware::class,
+        //     'type.ict_section_head' => \App\Http\Middleware\IctSectionHead::class,
+        //     'type.ict_group_leader' => \App\Http\Middleware\IctGroupLeader::class,
+        //     'type.ict_admin' => \App\Http\Middleware\IctAdmin::class,
+        //     'type.ict_technician' => \App\Http\Middleware\IctTechnician::class,
+        //     // 'type.ict_guest' => \App\Http\Middleware\AdminMiddleware::class,
+        // ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
-            if ($request->is('api/*')) {
-                return response()->json([
-                    'status' => 'false',
-                    'message' => 'Route Not found',
-                ], 404);
-            }
+        // $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+        //     if ($request->is('api/*')) {
+        //         return response()->json([
+        //             'status' => 'false',
+        //             'message' => 'Route Not found',
+        //         ], 404);
+        //     }
+        // });
+        $exceptions->render(function (Throwable $e) {
+            // dd($e);
+            // if($e->message == 'Unauthenticated') {
+            //     return 'okee';
+            // }
+            // if ($request->is('api/*')) {
+                // return response()->json([
+                //     'status' => 'false',
+                //     'message' => 'Route Not found',
+                // ], 404);
+            // }
+
+            return redirect("api/");
         });
     })->create();
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,27 @@
 <?php
 
-use App\Http\Controllers\KomputerController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Illuminate\Foundation\Application;
 
 Route::get('/', function () {
-    return inertia('index/index');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-// Route::apiResource('komputer', KomputerController::class);
-// Route::get('komputer_list', 'KomputerController@index');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
