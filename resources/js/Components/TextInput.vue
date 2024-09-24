aa9ed0c3f9400315fbd764300d6e4587e29a1f17
dikrix <dikriardiansyah@gmail.com>
2024-09-24 09:09:26 +0800

    layouting dashboard view

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -19,7 +19,7 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
     <input
-        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+        class="focus:shadow-primary-outline dark:bg-gray-950 dark:placeholder:text-white/80 dark:text-white/80 text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding p-3 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-fuchsia-300 focus:outline-none"
         v-model="model"
         ref="input"
     />
--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -31,47 +31,13 @@ const yearxyz = new Date().getFullYear();
                      class="items-center flex-grow transition-all duration-500 lg-max:overflow-hidden ease lg-max:max-h-0 basis-full lg:flex lg:basis-auto">
                      <ul class="flex flex-col pl-0 mx-auto mb-0 list-none lg:flex-row xl:ml-auto">
                         <li>
-                           <a class="flex items-center px-4 py-2 mr-2 font-normal transition-all ease-in-out lg-max:opacity-0 duration-250 text-sm text-slate-700 lg:px-2"
-                              aria-current="page" href="../pages/dashboard.html">
-                              <i class="mr-1 fa fa-home opacity-60"></i>
-                              Home
-                           </a>
+                           <span class="py-1.75 mr-4 ml-4 whitespace-nowrap font-bold text-slate-700 lg:ml-0 text-2xl">ICT PPA CENTER</span>
                         </li>
-                        <!-- <li>
-                           <a class="block px-4 py-2 mr-2 font-normal transition-all ease-in-out lg-max:opacity-0 duration-250 text-sm text-slate-700 lg:px-2"
-                              href="../pages/profile.html">
-                              <i class="mr-1 fa fa-user opacity-60"></i>
-                              Profile
-                           </a>
-                        </li> -->
-                        <!-- <li>
-                           <a class="block px-4 py-2 mr-2 font-normal transition-all ease-in-out lg-max:opacity-0 duration-250 text-sm text-slate-700 lg:px-2"
-                              href="../pages/sign-up.html">
-                              <i class="mr-1 fas fa-user-circle opacity-60"></i>
-                              Sign Up
-                           </a>
-                        </li>
-                        <li>
-                           <a class="block px-4 py-2 mr-2 font-normal transition-all ease-in-out lg-max:opacity-0 duration-250 text-sm text-slate-700 lg:px-2"
-                              href="../pages/sign-in.html">
-                              <i class="mr-1 fas fa-key opacity-60"></i>
-                              Sign In
-                           </a>
-                        </li> -->
+                       
                      </ul>
-                     <!-- online builder btn  -->
-                     <!-- <li class="flex items-center">
-                  <a
-                    class="leading-pro ease-in text-blue-500 border-blue-500 text-xs tracking-tight-rem bg-150 bg-x-25 rounded-3.5xl hover:border-blue-500 hover:-translate-y-px hover:text-blue-500 active:hover:border-blue-500 active:hover:-translate-y-px active:hover:text-blue-500 active:opacity-85 active:shadow-xs active:bg-blue-500 active:border-blue-500 mr-2 mb-0 inline-block cursor-pointer border border-solid bg-transparent py-2 px-8 text-center align-middle font-bold uppercase shadow-none transition-all hover:bg-transparent hover:opacity-75 hover:shadow-none active:scale-100 active:text-white active:hover:bg-transparent active:hover:opacity-75 active:hover:shadow-none"
-                    target="_blank"
-                    href="https://www.creative-tim.com/builder/soft-ui?ref=navbar-dashboard&amp;_ga=2.76518741.1192788655.1647724933-1242940210.1644448053"
-                    >Online Builder</a
-                  >
-                </li> -->
+
                      <ul class="hidden pl-0 mb-0 list-none lg:block lg:flex-row">
                         <li>
-                           <!-- <a href="#" target="_blank"
-                              class="inline-block px-8 py-2 mb-0 mr-1 font-bold leading-normal text-center text-white align-middle transition-all ease-in bg-blue-500 border-0 rounded-lg shadow-md cursor-pointer hover:-translate-y-px hover:shadow-xs active:opacity-85 text-xs tracking-tight-rem"><i class="fa fa fa-home"></i> Home</a> -->
                               <img src="/images/sinergyppa.png" alt="message placeholder image" width="80">
                         </li>
                      </ul>
@@ -112,31 +78,6 @@ const yearxyz = new Date().getFullYear();
    <footer class="py-12">
       <div class="container">
          <div class="flex flex-wrap -mx-3">
-            <!-- <div class="flex-shrink-0 w-full max-w-full mx-auto mb-6 text-center lg:flex-0 lg:w-8/12">
-               <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> Company </a>
-               <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> About Us </a>
-               <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> Team </a>
-               <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> Products </a>
-               <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> Blog </a>
-               <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12"> Pricing </a>
-            </div>
-            <div class="flex-shrink-0 w-full max-w-full mx-auto mt-2 mb-6 text-center lg:flex-0 lg:w-8/12">
-               <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
-                  <span class="text-lg fab fa-dribbble"></span>
-               </a>
-               <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
-                  <span class="text-lg fab fa-twitter"></span>
-               </a>
-               <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
-                  <span class="text-lg fab fa-instagram"></span>
-               </a>
-               <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
-                  <span class="text-lg fab fa-pinterest"></span>
-               </a>
-               <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
-                  <span class="text-lg fab fa-github"></span>
-               </a>
-            </div> -->
          </div>
          <div class="flex flex-wrap -mx-3">
             <div class="w-8/12 max-w-full px-3 mx-auto mt-1 text-center flex-0">
