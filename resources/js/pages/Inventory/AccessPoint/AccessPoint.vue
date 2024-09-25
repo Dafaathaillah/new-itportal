4fb950269047370f6b113c6d393544e980b9370d
dafaathaillah <dafabintang76@gmail.com>
2024-09-25 17:10:47 +0800

    setting page Access point table data

---
+++ resources/js/pages/Inventory/AccessPoint/AccessPoint.vue
@@ -0,0 +1,211 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
+import NavLink from "@/Components/NavLink.vue";
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <nav>
+                <!-- breadcrumb -->
+                <ol
+                    class="flex flex-wrap pt-1 mr-12 bg-transparent rounded-lg sm:mr-16"
+                >
+                    <li class="text-sm leading-normal">
+                        <a class="text-white opacity-50" href="javascript:;"
+                            >Pages</a
+                        >
+                    </li>
+                    <li
+                        class="text-sm pl-2 capitalize leading-normal text-white before:float-left before:pr-2 before:text-white before:content-['/']"
+                        aria-current="page"
+                    >
+                        Access Point Pages
+                    </li>
+                </ol>
+                <h6 class="mb-0 font-bold text-white capitalize">
+                    Access Point Data
+                </h6>
+            </nav>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex flex-wrap -mx-3">
+                    <div class="flex-none w-full max-w-full px-3">
+                        <div
+                            class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border"
+                        >
+                            <div
+                                class="p-6 pb-0 mb-0 border-b-0 border-b-solid rounded-t-2xl border-b-transparent"
+                            >
+                                <NavLink
+                                    :href="route('accessPoint.create')"
+                                    class="inline-block px-5 py-2.5 font-bold leading-normal text-center text-white align-middle transition-all bg-transparent rounded-lg cursor-pointer text-sm ease-in shadow-md bg-150 bg-gradient-to-tl from-zinc-800 to-zinc-700 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 hover:shadow-xs active:opacity-85 hover:-translate-y-px tracking-tight-rem bg-x-25"
+                                >
+                                    <i class="fas fa-plus"> </i>&nbsp;&nbsp;Add
+                                    New Data
+                                </NavLink>
+                            </div>
+                            <div class="flex-auto px-0 pt-0 pb-2">
+                                <div class="p-0 overflow-x-auto">
+                                    <table
+                                        class="items-center w-full mb-0 align-top border-collapse dark:border-white/40 text-slate-500"
+                                    >
+                                        <thead class="align-bottom">
+                                            <tr>
+                                                <th
+                                                    class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-collapse shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70"
+                                                >
+                                                    #
+                                                </th>
+                                                <th
+                                                    class="px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b border-collapse shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70"
+                                                >
+                                                    Device Name
+                                                </th>
+                                                <th
+                                                    class="px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b border-collapse shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70"
+                                                >
+                                                    Device Type
+                                                </th>
+                                                <th
+                                                    class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-collapse shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70"
+                                                >
+                                                    Device Status
+                                                </th>
+                                                <th
+                                                    class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-collapse shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70"
+                                                >
+                                                    Inventory At
+                                                </th>
+                                                <th
+                                                    class="px-6 py-3 font-semibold capitalize align-middle bg-transparent border-b border-collapse border-solid shadow-none dark:border-white/40 dark:text-white tracking-none whitespace-nowrap text-slate-400 opacity-70"
+                                                >
+                                                    Action
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td
+                                                    class="p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
+                                                >
+                                                    <span
+                                                        class="text-xs font-semibold leading-tight dark:text-white dark:opacity-80 text-slate-400"
+                                                        >1
+                                                    </span>
+                                                </td>
+                                                <td
+                                                    class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
+                                                >
+                                                    <p
+                                                        class="mb-0 text-sm font-semibold leading-tight dark:text-white dark:opacity-80"
+                                                    >
+                                                        Access Point
+                                                    </p>
+                                                </td>
+                                                <td
+                                                    class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
+                                                >
+                                                    <p
+                                                        class="mb-0 text-sm font-semibold leading-tight dark:text-white dark:opacity-80"
+                                                    >
+                                                        Ubiquity
+                                                    </p>
+                                                </td>
+                                                <td
+                                                    class="p-2 text-sm leading-normal text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
+                                                >
+                                                    <span
+                                                        class="bg-gradient-to-tl from-emerald-500 to-teal-400 px-2.5 text-xs rounded-1.8 py-1.4 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white"
+                                                        >Online</span
+                                                    >
+                                                </td>
+                                                <td
+                                                    class="p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
+                                                >
+                                                    <span
+                                                        class="text-xs font-semibold leading-tight dark:text-white dark:opacity-80 text-slate-400"
+                                                        >23/04/18</span
+                                                    >
+                                                </td>
+                                                <td
+                                                    class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
+                                                >
+                                                    <a
+                                                        href="javascript:;"
+                                                        class="text-xs font-semibold leading-tight dark:text-white dark:opacity-80 text-slate-400"
+                                                    >
+                                                        Edit
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td
+                                                    class="p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
+                                                >
+                                                    <span
+                                                        class="text-xs font-semibold leading-tight dark:text-white dark:opacity-80 text-slate-400"
+                                                        >1
+                                                    </span>
+                                                </td>
+                                                <td
+                                                    class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
+                                                >
+                                                    <p
+                                                        class="mb-0 text-sm font-semibold leading-tight dark:text-white dark:opacity-80"
+                                                    >
+                                                        Access Point
+                                                    </p>
+                                                </td>
+                                                <td
+                                                    class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
+                                                >
+                                                    <p
+                                                        class="mb-0 text-sm font-semibold leading-tight dark:text-white dark:opacity-80"
+                                                    >
+                                                        Ubiquity
+                                                    </p>
+                                                </td>
+                                                <td
+                                                    class="p-2 text-sm leading-normal text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
+                                                >
+                                                    <span
+                                                        class="bg-gradient-to-tl from-emerald-500 to-teal-400 px-2.5 text-xs rounded-1.8 py-1.4 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white"
+                                                        >Online</span
+                                                    >
+                                                </td>
+                                                <td
+                                                    class="p-2 text-center align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
+                                                >
+                                                    <span
+                                                        class="text-xs font-semibold leading-tight dark:text-white dark:opacity-80 text-slate-400"
+                                                        >23/04/18</span
+                                                    >
+                                                </td>
+                                                <td
+                                                    class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent"
+                                                >
+                                                    <a
+                                                        href="javascript:;"
+                                                        class="text-xs font-semibold leading-tight dark:text-white dark:opacity-80 text-slate-400"
+                                                    >
+                                                        Edit
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
