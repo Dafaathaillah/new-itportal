582a51e4d65641ec75b0e471a62d3677dd3dab36
dafaathaillah <dafabintang76@gmail.com>
2024-10-06 08:34:42 +0800

    update add mac address

--- app/Imports/SwitchImport.php
+++ app/Imports/SwitchImport.php
@@ -14,28 +14,30 @@ class SwitchImport implements ToModel
     */
     public function startCell(): string
     {
-        return 'B2'; // Mulai dari sel B3, sesuaikan dengan kebutuhanmu
+        return 'A17'; // Mulai dari sel B3, sesuaikan dengan kebutuhanmu
     }
 
     public function startRow(): int
     {
-        return 2; // Mulai dari baris kedua
+        return 17; // Mulai dari baris kedua
     }
 
     public function model(array $row)
     {
+        dd($row);
         return new InvSwitch([
-            'max_id' => 2,
-            'device_name' => $row[0],
-            'inventory_number' => $row[1],
-            'serial_number' => $row[2],
-            'ip_address' => $row[3],
-            'device_brand' => $row[4],
-            'device_type' => $row[5],
-            'device_model' => $row[6],
-            'location' => $row[7],
-            'status' => $row[8],
-            'note' => $row[9],
+            'max_id' => $row[0],
+            'device_name' => $row[1],
+            'inventory_number' => $row[2],
+            'serial_number' => $row[3],
+            'ip_address' => $row[4],
+            'mac_address' => $row[5],
+            'device_brand' => $row[6],
+            'device_type' => $row[7],
+            'device_model' => $row[8],
+            'location' => $row[9],
+            'status' => $row[10],
+            'note' => $row[11],
         ]);
     }
 }
